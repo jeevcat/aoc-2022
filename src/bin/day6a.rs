ebf41d2106ac1c769aecedd006d562c6d14d2628
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-12-13 15:22:26 +0100

    Day 6

---
+++ src/bin/day6a.rs
@@ -0,0 +1,45 @@
+use std::{collections::HashMap, fs};
+
+const WINDOW_SIZE: usize = 4;
+
+fn main() {
+    let signal = fs::read_to_string("input6.txt").unwrap();
+    let start = find_start(&signal);
+    dbg!(start);
+}
+
+fn find_start(signal: &str) -> usize {
+    let mut prev: HashMap<u8, u32> = HashMap::new();
+    let mut duplicates = 0;
+    for c in signal.as_bytes().iter().take(WINDOW_SIZE) {
+        let new = prev.entry(*c).or_default();
+        if *new == 1 {
+            duplicates += 1;
+        }
+        *new += 1;
+    }
+    dbg!(duplicates);
+
+    signal
+        .as_bytes()
+        .windows(WINDOW_SIZE + 1)
+        .position(|win| {
+            dbg!(duplicates);
+            let found = duplicates == 0;
+
+            let new = prev.entry(*win.last().unwrap()).or_default();
+            if *new == 1 {
+                duplicates += 1;
+            }
+            *new += 1;
+
+            let old = prev.entry(*win.first().unwrap()).or_default();
+            *old -= 1;
+            if *old == 1 {
+                duplicates -= 1;
+            }
+            found
+        })
+        .unwrap()
+        + WINDOW_SIZE
+}
