e986fdbab6aaa4f6ca05d0754b0dfa7a43c93ad3
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-12-13 11:21:44 +0100

    Day 5

---
+++ src/bin/day5b.rs
@@ -0,0 +1,90 @@
+use std::{
+    fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader},
+    str::FromStr,
+};
+
+struct Move {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+impl FromStr for Move {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut i = s.split(char::is_whitespace);
+        let count = i.nth(1).unwrap().parse().unwrap();
+        let from = i.nth(1).unwrap().parse().unwrap();
+        let to = i.nth(1).unwrap().parse().unwrap();
+        Ok(Self { count, from, to })
+    }
+}
+
+struct Stacks(Vec<Vec<char>>);
+
+impl Stacks {
+    fn new(lines: &[String]) -> Self {
+        let mut i = lines.iter().rev();
+        let size = i.next().unwrap().split_whitespace().count();
+        let mut stacks = vec![vec![]; size];
+        for line in i {
+            let mut j = line.chars().skip(1);
+            let mut index = 0;
+            while let Some(c) = j.next() {
+                if c.is_alphabetic() {
+                    stacks[index].push(c);
+                }
+                index += 1;
+                j.nth(2); //skip to next letter
+            }
+        }
+
+        Self(stacks)
+    }
+
+    fn apply(&mut self, m: &Move) {
+        // set the size of the target stack
+        self.0[m.to - 1].extend(std::iter::repeat('*').take(m.count));
+
+        // write backwards to the target stack
+        let end = self.0[m.to - 1].len();
+        let start = end - m.count;
+        for i in (start..end).rev() {
+            let popped = self.0[m.from - 1].pop().unwrap();
+            self.0[m.to - 1][i] = popped;
+        }
+    }
+}
+
+impl Display for Stacks {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for stack in self.0.iter() {
+            write!(f, "{}", stack.last().unwrap_or(&'_'))?;
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    let file = File::open("input5.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut lines = reader.lines().flatten();
+    let mut stacks = vec![];
+    for line in lines.by_ref() {
+        if line.is_empty() {
+            break;
+        }
+        stacks.push(line);
+    }
+
+    let mut stacks = Stacks::new(&stacks);
+
+    for mv in lines.map(|line| line.parse().unwrap()) {
+        stacks.apply(&mv);
+    }
+    println!("{stacks}");
+}
